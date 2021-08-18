37bab70fece69c4035013aa21af2f1bf48640874
Leandro Aguiar <laleandroaguiar@gmail.com>
2021-08-18 17:40:58 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -15,9 +15,9 @@ namespace SalesWebMvc.Models
         }
 
         public DbSet<Department> Department { get; set; }
-        /*
+        
         public DbSet<Seller> Seller { get; set; }
         public DbSet<SalesRecord> SalesRecord { get; set; }
-*/
+
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-
-
-
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new LinkedList<Seller>();
 
         public Department()
         {
@@ -21,6 +19,16 @@ namespace SalesWebMvc.Models
         {
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
 }
