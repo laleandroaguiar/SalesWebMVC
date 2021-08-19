67e4e353eb021d5f40c5a432a90e3e90be882c8d
Leandro Aguiar <laleandroaguiar@gmail.com>
2021-08-19 17:54:15 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SalesWebMvc.Services.Exceptions;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
